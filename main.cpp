e9c5c81939228d69c41ad3f2e27a684c41e175a5
MariamEmadEldeen <117228754+MariamEmadEldeen@users.noreply.github.com>
2023-01-03 10:51:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,14 +31,7 @@ float rx=0,ry=0;
 
 //------------------------------  reshapeFunc  ---------------------------------
 
-void reshapeFunc (int w, int h)
-{
-    glViewport(0,0,(GLsizei)w,(GLsizei)h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective (40.0, (GLdouble)w / (GLdouble)h, 0.5, 20.0);
-    glMatrixMode(GL_MODELVIEW);
-}
+
 
 //------------------------------  display   -------------------------------
 void moon(){
